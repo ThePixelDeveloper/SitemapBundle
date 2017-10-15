10aba15943ea8866793a65775a2dd02eb5875f50
Mathew Davies <thepixeldeveloper@icloud.com>
2017-10-15 15:49:39 +0100

    Create a configuration for the sitemap files to live in.

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Thepixeldeveloper\SitemapBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('thepixeldeveloper_sitemap');
+
+        $rootNode
+            ->children()
+                ->scalarNode('sitemap_directory')
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- src/DependencyInjection/SitemapExtension.php
+++ src/DependencyInjection/SitemapExtension.php
@@ -13,5 +13,8 @@ class SitemapExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
     }
 }
