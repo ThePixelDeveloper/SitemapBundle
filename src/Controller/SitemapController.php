5e5767177214c427b19186515391bca57ac93e3d
Mathew Davies <thepixeldeveloper@icloud.com>
2017-11-11 19:52:44 +0000

    Configure controller to serve sitemap files.

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -3,13 +3,39 @@
 namespace Thepixeldeveloper\SitemapBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class SitemapController extends Controller
 {
-    public function sitemapAction(Request $request): Response
+    /**
+     * @var string
+     */
+    private $directory;
+
+    /**
+     * SitemapController constructor.
+     *
+     * @param string $directory
+     */
+    public function __construct(string $directory)
     {
-        
+        $this->directory = $directory;
+    }
+
+    /**
+     * @return Response
+     */
+    public function sitemapAction(): Response
+    {
+        $response = new BinaryFileResponse(rtrim($this->directory, '/') . '/sitemap-0.xml');
+        $response->headers->set('Content-Type', 'application/xml');
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_INLINE,
+            'sitemap.xml'
+        );
+
+        return $response;
     }
 }
