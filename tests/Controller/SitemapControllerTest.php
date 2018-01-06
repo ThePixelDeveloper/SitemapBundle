fda85d345139ae04f07fcc852b888333291c580d
Mathew Davies <thepixeldeveloper@icloud.com>
2018-01-06 21:20:33 +0000

    Update the Controller test to use a mock filesystem.

--- tests/Controller/SitemapControllerTest.php
+++ tests/Controller/SitemapControllerTest.php
@@ -2,17 +2,35 @@
 
 namespace Thepixeldeveloper\SitemapBundle\Controller;
 
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 
 class SitemapControllerTest extends TestCase
 {
+    /**
+     * @var vfsStreamDirectory
+     */
+    protected $filesystem;
+
+    /**
+     * Virtual filesystem.
+     */
+    protected function setUp()
+    {
+        $this->filesystem = vfsStream::setup('sitemaps', null, [
+            'sitemap.xml'  => 'content',
+            'urlset-0.xml' => 'content',
+        ]);
+    }
+
     public function testSitemapAction()
     {
         $request = $this->getMockBuilder(Request::class)->getMock();
         $request->expects($this->once())->method('get')->with('sitemap')->willReturn('sitemap');
 
-        $controller = new SitemapController(__DIR__ . '/resources/sitemaps');
+        $controller = new SitemapController($this->filesystem->url());
         $response = $controller->sitemapAction($request);
 
         $this->assertSame('application/xml', $response->headers->get('Content-Type'));
@@ -24,7 +42,7 @@ class SitemapControllerTest extends TestCase
         $request = $this->getMockBuilder(Request::class)->getMock();
         $request->expects($this->once())->method('get')->with('sitemap')->willReturn('urlset-0');
 
-        $controller = new SitemapController(__DIR__ . '/resources/sitemaps');
+        $controller = new SitemapController($this->filesystem->url());
         $response = $controller->sitemapAction($request);
 
         $this->assertSame('application/xml', $response->headers->get('Content-Type'));
