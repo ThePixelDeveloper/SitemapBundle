3dec2411eab9ecd7603afbe3f8b7c65209ecd0ba
Mathew Davies <thepixeldeveloper@icloud.com>
2018-01-07 14:34:28 +0000

    Update maintainability of the SitemapControllerTest.

--- tests/Controller/SitemapControllerTest.php
+++ tests/Controller/SitemapControllerTest.php
@@ -1,11 +1,12 @@
 <?php declare(strict_types=1);
 
-namespace Thepixeldeveloper\SitemapBundle\Controller;
+namespace Tests\Thepixeldeveloper\SitemapBundle\Controller;
 
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamDirectory;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Thepixeldeveloper\SitemapBundle\Controller\SitemapController;
 
 class SitemapControllerTest extends TestCase
 {
@@ -25,27 +26,34 @@ class SitemapControllerTest extends TestCase
         ]);
     }
 
-    public function testSitemapAction()
+    public function providerRequests(): \Generator
     {
-        $request = $this->getMockBuilder(Request::class)->getMock();
-        $request->expects($this->once())->method('get')->with('sitemap')->willReturn('sitemap');
+        yield [
+            new Request([], ['sitemap' => 'sitemap'] ,[], [], [], []),
+            'application/xml',
+            'inline; filename="sitemap.xml"',
+        ];
 
-        $controller = new SitemapController($this->filesystem->url());
-        $response = $controller->sitemapAction($request);
-
-        $this->assertSame('application/xml', $response->headers->get('Content-Type'));
-        $this->assertSame('inline; filename="sitemap.xml"', $response->headers->get('Content-Disposition'));
+        yield [
+            new Request([], ['sitemap' => 'urlset-0'] ,[], [], [], []),
+            'application/xml',
+            'inline; filename="urlset-0.xml"',
+        ];
     }
 
-    public function testUrlsetSitemapAction()
+    /**
+     * @dataProvider providerRequests
+     *
+     * @param Request $request
+     * @param string  $contentType
+     * @param string  $contentDisposition
+     */
+    public function testSitemapAction(Request $request, string $contentType, string $contentDisposition)
     {
-        $request = $this->getMockBuilder(Request::class)->getMock();
-        $request->expects($this->once())->method('get')->with('sitemap')->willReturn('urlset-0');
-
         $controller = new SitemapController($this->filesystem->url());
         $response = $controller->sitemapAction($request);
 
-        $this->assertSame('application/xml', $response->headers->get('Content-Type'));
-        $this->assertSame('inline; filename="urlset-0.xml"', $response->headers->get('Content-Disposition'));
+        $this->assertSame($contentType, $response->headers->get('Content-Type'));
+        $this->assertSame($contentDisposition, $response->headers->get('Content-Disposition'));
     }
 }
