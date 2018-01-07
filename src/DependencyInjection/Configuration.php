1990139f97a13beb1bba321fc67ec05709fdb505
Mathew Davies <thepixeldeveloper@icloud.com>
2018-01-07 14:34:48 +0000

    Type hinting for the Configuration clas.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Thepixeldeveloper\SitemapBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -10,6 +11,10 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+
+        /**
+         * @var ArrayNodeDefinition $rootNode
+         */
         $rootNode = $treeBuilder->root('thepixeldeveloper_sitemap');
 
         $rootNode
